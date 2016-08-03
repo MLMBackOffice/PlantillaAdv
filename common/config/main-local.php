0c55efa33ca6119b6cd1dd652bfadd5416070b1c
ivan <ivan-salazar@hotmail.com>
2016-08-02 23:06:01 -0500

    se adiciona enlace compra (parcial)

--- common/config/main-local.php
+++ common/config/main-local.php
@@ -15,6 +15,19 @@ return [
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
-        ],
+        ],          
+        'urlManager' => [
+            'class' => 'yii\web\UrlManager',
+            // Disable index.php
+            'showScriptName' => false,
+            // Disable r= routes
+            'enablePrettyUrl' => true,
+            'rules' => array(
+                '<controller:\w+>/<id:\d+>' => '<controller>/view',
+                '<controller:\w+>/<action:\w+>/<id:\d+>' => '<controller>/<action>',
+                '<controller:\w+>/<action:\w+>' => '<controller>/<action>',
+    //            '<controller:\w+>/<action:\w+>/<patrocinador:\w+>' => '<controller>/<action>/<patrocinador>',
+            ),
+        ]
     ],
 ];
