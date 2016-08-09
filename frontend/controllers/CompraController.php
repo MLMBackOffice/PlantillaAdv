fab040a48d2ae3384f83090e5266d36e6ba16d85
admin <admin@localhost>
2016-08-09 17:40:54 -0500

    intento curl

--- frontend/controllers/CompraController.php
+++ frontend/controllers/CompraController.php
@@ -11,17 +11,17 @@ use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
 use dosamigos\qrcode\formats\MailTo;
 use dosamigos\qrcode\QrCode;
-use linslin\yii2\curl;
+use vendor\linslin\Curl;
+
 /**
  * CompraController implements the CRUD actions for Compra model.
  */
-class CompraController extends Controller
-{
+class CompraController extends Controller {
+
     /**
      * @inheritdoc
      */
-    public function behaviors()
-    {
+    public function behaviors() {
         return [
             'verbs' => [
                 'class' => VerbFilter::className(),
@@ -31,30 +31,28 @@ class CompraController extends Controller
             ],
         ];
     }
-    
+
     public function actionQrcode() {
-    $mailTo = new MailTo(['email' => 'ivan-salazar@hotmail.com']);
-    $bit = new Bitcoin();
-    $bit->address ='1AfMEZLAGGimTHunNpvJ1BVTnRWFhMEewr';
-    $bit->amount ='0.053';
-    return QrCode::png($bit->getText());// return QrCode::png('$mailTo->getText()');
-  
-    // you could also use the following
-    // return return QrCode::png($mailTo);
-}   
+        $mailTo = new MailTo(['email' => 'ivan-salazar@hotmail.com']);
+        $bit = new Bitcoin();
+        $bit->address = '1AfMEZLAGGimTHunNpvJ1BVTnRWFhMEewr';
+        $bit->amount = '0.053';
+        return QrCode::png($bit->getText()); // return QrCode::png('$mailTo->getText()');
+        // you could also use the following
+        // return return QrCode::png($mailTo);
+    }
 
     /**
      * Lists all Compra models.
      * @return mixed
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
         $searchModel = new CompraSearch();
         $dataProvider = $searchModel->search(Yii::$app->request->queryParams);
 
         return $this->render('/compra/index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+                    'searchModel' => $searchModel,
+                    'dataProvider' => $dataProvider,
         ]);
     }
 
@@ -63,10 +61,9 @@ class CompraController extends Controller
      * @param integer $id
      * @return mixed
      */
-    public function actionView($id)
-    {
+    public function actionView($id) {
         return $this->render('view', [
-            'model' => $this->findModel($id),
+                    'model' => $this->findModel($id),
         ]);
     }
 
@@ -75,70 +72,99 @@ class CompraController extends Controller
      * If creation is successful, the browser will be redirected to the 'view' page.
      * @return mixed
      */
-    public function actionCreate()
-    {
+    public function actionCreate() {
         $model = new Compra();
-        
+        //  empresa::findOne($condition)
         if (($empresa = Empresa::findOne('1')) == null) {
-           
+
             throw new NotFoundHttpException('The company  does not exist.');
         }
-        
 
-        if ($model->load(Yii::$app->request->post()) ) {
-         $model->empresa_id= $empresa->Id;
-            $model->id_usuario=Yii::$app->user->id;
-            $model->fecha_registro=date('Y-m-d h:m:s');
-             $model->save();
-             
-             
-                $curl = new curl\Curl();
- 
-        //post http://example.com/
-        $response = $curl->setOption(
-                CURLOPT_POSTFIELDS, 
-                http_build_query(array(
-                    'type' => 'int',
-                    'txid' => '54345f6a-3f3a-4376-abb3-8bd8c7f6d3d6'
-                )
-            ))
-            ->post('https://tropay.com/v0/secure-api/get-usd-balance');
-return $response;
-        //    return $this->redirect(array('https://tropay.com/v0/secure-api/get-usd-balance', 'type' => 'int', 'txid' => '54345f6a-3f3a-4376-abb3-8bd8c7f6d3d6'));
-            
-            /*return $this->render('//compra/_form',  
-            [ 'model' => $model,]);*/
+
+        if ($model->load(Yii::$app->request->post())) {
+            $model->empresa_id = $empresa->Id;
+            $model->id_usuario = Yii::$app->user->id;
+            $model->fecha_registro = date('Y-m-d h:m:s');
+            $model->estado = '0';
+            $model->save();
+
+
+
+            $postData = array('type' => 'int', 'txid' => '54345f6a-3f3a-4376-abb3-8bd8c7f6d3d6');
+            $url = "https://tropay.com/v0/secure-api/get-usd-balance";
+            /* Convierte el array en el formato adecuado para cURL */
+            $elements = array();
+            foreach ($postData as $name => $value) {
+                $elements[] = "{$name}=" . urlencode($value);
+            }
+            $handler = curl_init();
+            curl_setopt($handler, CURLOPT_URL, $url);
+            curl_setopt($handler, CURLOPT_POST, true);
+            curl_setopt($handler, CURLOPT_POSTFIELDS, $elements);
+            curl_setopt($handler, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($handler, CURLOPT_SSL_VERIFYHOST, false);
+            curl_setopt ($handler, CURLOPT_CAINFO, "/etc/cacert.pem");
+            curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+           $response = curl_exec($handler);
+
+       /*    if (curl_exec($handler) === false) {
+                return 'Curl error: ' . curl_error($handler);
+            } else {
+                return 'Operación completada sin errores';
+            }*/
+            curl_close($handler);
+          //  var_dump(json_decode($response, true));
+           return json_decode($response, true);
+
+
+
+
+
+            /*      $curl = new Curl();
+
+
+              $response = $curl->setOption(
+              CURLOPT_POSTFIELDS,
+              http_build_query(array(
+              'type' => 'int',
+              'txid' => '54345f6a-3f3a-4376-abb3-8bd8c7f6d3d6'
+              )
+              ))
+              ->post('https://tropay.com/v0/secure-api/get-usd-balance');
+              return $response; */
+            //    return $this->redirect(array('https://tropay.com/v0/secure-api/get-usd-balance', 'type' => 'int', 'txid' => '54345f6a-3f3a-4376-abb3-8bd8c7f6d3d6'));
+
+            return $this->render('//compra/_form', [ 'model' => $model, 'empresa' => $empresa,]);
         } else {
             return $this->render('//compra/_form', [
-                'model' => $model,
-                'empresa' => $empresa,
+                        'model' => $model,
+                        'empresa' => $empresa,
             ]);
         }
     }
-    
-      public function actionVercompra()
-    {
+
+    public function actionVercompra() {
         $model = new Compra();
-        
-         
-     /*    return $this->renderAjax('//compra/modal_compra',  
-            [ 'model' => $model,]);*/
+
+
+        /*    return $this->renderAjax('//compra/modal_compra',  
+          [ 'model' => $model,]); */
     }
+
     /**
      * Updates an existing Compra model.
      * If update is successful, the browser will be redirected to the 'view' page.
      * @param integer $id
      * @return mixed
      */
-    public function actionUpdate($id)
-    {
+    public function actionUpdate($id) {
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
             return $this->redirect(['view', 'id' => $model->id_compra]);
         } else {
             return $this->render('update', [
-                'model' => $model,
+                        'model' => $model,
             ]);
         }
     }
@@ -149,8 +175,7 @@ return $response;
      * @param integer $id
      * @return mixed
      */
-    public function actionDelete($id)
-    {
+    public function actionDelete($id) {
         $this->findModel($id)->delete();
 
         return $this->redirect(['index']);
@@ -163,12 +188,12 @@ return $response;
      * @return Compra the loaded model
      * @throws NotFoundHttpException if the model cannot be found
      */
-    protected function findModel($id)
-    {
+    protected function findModel($id) {
         if (($model = Compra::findOne($id)) !== null) {
             return $model;
         } else {
             throw new NotFoundHttpException('The requested page does not exist.');
         }
     }
+
 }
--- frontend/views/compra/_form.php
+++ frontend/views/compra/_form.php
@@ -3,7 +3,7 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use yii\helpers\ArrayHelper;
-use backend\models\paquetes;
+use backend\models\Paquetes;
 use yii\helpers\Url;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Compra */
@@ -18,6 +18,14 @@ use yii\helpers\Url;
                                                style="font-size: 25px"></i>   Pay with Tropay</button></div>
     <div class="flat-green"><button id="btn-tropay" data-publicKey="eyJpdiI6IkdFaFVNaTY3ZUF4UzA1STJwVGNkWEE9PSIsInZhbHVlIjoibFozYmF4QXF3QVpTUVl6TmlQWExxQnBWZ1BMOGdPdVNWcmRFSDRnbGNEMk1hbEtnMlpHOGJJRVpNc2RocGlQSyIsIm1hYyI6IjU1NzRiNGFhNDA4OGY5NGUzMTBmM2ExOGVlYTA3OGY3NDFlZDNlMmI5NTAxZDI5ZDdhZDRhZWRiNmRkY2JiMmEifQ==" data-title="Compra libro" data-importe="150" data-ref="4478" style="width: 220px ; height: 50px; border-radius: 10px; font-size: 20px" class="btn btn-success btn-block"><i class="icon fa fa-bitcoin  pull-left" style="font-size: 25px"></i>   Pay with Tropay</button></div>
      <?= $form->field($model, 'empresa_id')->hiddenInput() ?>
+ * 
+ * 
+ *     <?= $form->field($model, 'id_paquete')->dropDownList(
+         ArrayHelper::map(Paquetes::find()->all(), 'id_paquete', 'nombre'),
+            ['prompt' => 'Seleccione',
+              'onchange'=>'alert("aaa");'   
+                ]
+          );?>
 
  */
   ?>
@@ -30,22 +38,23 @@ use yii\helpers\Url;
                 ]
     ]); 
     echo Html::hiddenInput('carId', $empresa->cardId);
+    
+    echo $form->field($model, 'id_paquete')->radioList(  ArrayHelper::map(Paquetes::find()->all(), 'id_paquete', 'nombre'),
+            ['prompt' => 'Seleccione',
+              'onchange'=>'alert("aaa");'   
+                ]);
+
     ?>
 
  
-    <?= $form->field($model, 'id_paquete')->dropDownList(
-         ArrayHelper::map(paquetes::find()->all(), 'id_paquete', 'nombre'),
-            ['prompt' => 'Seleccione',
-              'onchange'=>'alert("aaa");'   
-                ]
-          );?>
+
 
 
     
 <!--<img src="https://chart.googleapis.com/chart?cht=qr&chs=250x250&cht=qr&chl=bitcoin:1AfMEZLAGGimTHunNpvJ1BVTnRWFhMEewr?amount=0.053" alt="qrcode"/>
     -->
   <?= Html::button('Create New Company', ['value' => Url::to(['compra/vercompra']), 'title' => 'Creating New Company', 'class' => 'showModalButton btn btn-success']); ?>
-  <?= Html::button('Compra', [ 'id' => 'verCodigo', 'class' => 'showModalButton btn btn-success']); ?>
+
    <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
